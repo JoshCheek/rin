8ad92dcc347362494df66641bdd20b0dbe4444b1
Josh Cheek <josh.cheek@gmail.com>
2015-04-22 00:13:21 -0600

    Move the spec helpers to the spec that uses them
    No need to split them out like they were,
    since they're only used in one place, stick them there.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,8 +3,26 @@ require 'spec_helper'
 # error cases:
 #   no program
 #   invalid base
+spec_helpers = Module.new do
+  def matcher_for(str_or_regex)
+    case str_or_regex
+    when Regexp then match str_or_regex
+    else             eq    str_or_regex
+    end
+  end
+
+  def runs!(*program, out:"", err:"", status:0)
+    actual_out, actual_err, actual_status = Open3.capture3(*program)
+
+    expect(actual_err).to matcher_for(err)
+    expect(actual_out).to matcher_for(out)
+    expect(actual_status.exitstatus).to eq status
+  end
+end
 
 RSpec.describe 'rin' do
+  include spec_helpers
+
   it 'prints the inspection of whatever the script evaluates to' do
     runs! 'rin', '"hello"', out: %'"hello"\n'
   end
@@ -39,6 +57,8 @@ end
 
 
 RSpec.describe 'ruby -rin -e' do
+  include spec_helpers
+
   it 'defaults the base to hex' do
     runs! 'ruby', '-rin', '-e', 'p 7 + 7', out: "E\n"
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,29 +1,11 @@
 require 'open3'
 
-module SpecHelpers
-  def matcher_for(str_or_regex)
-    case str_or_regex
-    when Regexp then match str_or_regex
-    else             eq    str_or_regex
-    end
-  end
-
-  def runs!(*program, out:"", err:"", status:0)
-    actual_out, actual_err, actual_status = Open3.capture3(*program)
-
-    expect(actual_err).to matcher_for(err)
-    expect(actual_out).to matcher_for(out)
-    expect(actual_status.exitstatus).to eq status
-  end
-end
-
 lib_path       = File.expand_path '../lib', __dir__
 bin_path       = File.expand_path '../bin', __dir__
 ENV['PATH']    = "#{bin_path}:#{ENV['PATH']}"
 ENV['RUBYOPT'] = "-I #{lib_path} #{ENV['RUBYOPT']}"
 
 RSpec.configure do |config|
-  config.include SpecHelpers
   config.disable_monkey_patching!
   config.fail_fast = true
 end
